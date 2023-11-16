f920c907ee0965976198ac55f112a63747dc56f8
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2023-11-16 11:49:17 -0500

    Added a bunch

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from config import RAPIDAPI_KEY, RAPIDAPI_HOST, API_STARTER, IEX_LAT_LON, IEX_AP
 import requests
 #This is where get_json_response comes from
 from functions import *
+import datetime
 
 #Initializing API address and API Key
 
@@ -20,45 +21,56 @@ app = FastAPI()
 # Obtain Refresh Times (In Seconds)
 @app.get("/times")
 async def refresh_times():
+    log_endpoint_access(logger, "times")
     return {"ADS-B Calls": INTERVAL_SECONDS}
+    
 
 
 #Homepage, this returns IEX HQ
 @app.get("/get_data")
 async def index():
+    log_endpoint_access(logger, "get_data")
     return get_json_response(IEX_API_URL, ads_headers)
+
 
 #MilPlane, This returns Milplane
 @app.get("/milplane")
 async def MilPlane():
+    log_endpoint_access(logger, "milplane")
     return get_json_response(API_STARTER + "/mil/", ads_headers)
 
 # This returns aircraft around the selected number of 
 # nautical (nm) miles
 @app.get("/dist/{int_nm}")
 async def dist(int_nm: int):
+    log_endpoint_access(logger, "dist")
     return get_json_response(API_STARTER + IEX_LAT_LON + f"/dist/{int_nm}", 
                             ads_headers)                         
 
 # This returns aircraft based on the inputed icao
 @app.get("/icao/{icao}")
 async def icao(icao: str):
+    log_endpoint_access(logger, "icao")
     return get_json_response(API_STARTER + f"/icao/{icao}", ads_headers)
 
 # This returns aircraft based on the inputed hex
 @app.get("/hex/{hex}")
 async def hex(hex: str):
+    log_endpoint_access(logger, "hex")
     return get_json_response(API_STARTER + f"/hex/{hex}", ads_headers)
 
 # This returns aircraft based on the inputed squawk code
 @app.get("/squawk/{squawk}")
 async def squawk(squawk: str):
+    log_endpoint_access(logger, "squawk")
     return get_json_response(API_STARTER + f"/sqk/{squawk}", 
                                 ads_headers)
 
 # This returns aircraft based on the inputed squawk code
 @app.get("/callsign/{callsign}")
 async def callsign(callsign: str):
+    log_endpoint_access(logger, "callsign")
     return get_json_response(API_STARTER + f"/callsign/{callsign}", 
                                 ads_headers)
+
 
