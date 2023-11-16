e20ec267d4c2de11736f02a8574c6db3e4c16dcd
Brandon Ly <89619512+BrandonSchoolPF@users.noreply.github.com>
2023-11-16 11:40:12 -0500

    Added a bunch

---
+++ functions.py
@@ -0,0 +1,44 @@
+#This is designed to hold all of our functions
+from fastapi import FastAPI, HTTPException
+import requests
+import logging
+import datetime;
+
+
+#Obtaining the json responses <- get_json_response
+def get_json_response(url, headers):
+    url = f"{url}"
+ 
+    # Make an HTTP GET request to the API with the specified headers
+    response = requests.get(url, headers=headers)
+ 
+    if response.status_code == 200:
+        try:
+            # Return a JSON response to view in browser
+            return response.json()
+        except ValueError:
+            raise HTTPException(status_code=500, detail="Response is not valid JSON.")
+    else:
+        # Raise an exception with the appropriate status code and error message
+        raise HTTPException(status_code=response.status_code, detail=f"Request failed with status code: {response.status_code}")
+
+
+#Logging Function
+def configure_logging(): #This sets the logger level
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    return logging.getLogger(__name__)
+
+logger = configure_logging()
+
+def log_endpoint_access(logger, endpoint_name): #This calls logger with endpoint
+    #Initiating ct which calls system time
+    ct = datetime.datetime.now()
+
+    #ip_address = request.client.host
+    #logger.info(f"Source IP: {ip_address}")
+
+    #Actual Logging info
+    logger.info(f" Making API call to: {endpoint_name} ") #Shows that it was successful
+    logger.info(f" Accessing {endpoint_name} endpoint Successful") #What is accessed
+    print(f"Accessed the {endpoint_name} endpoint at", ct) #Time accessed
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 import requests
+<<<<<<< Updated upstream
+=======
+#This is where get_json_response comes from
+from functions import *
+import datetime
+>>>>>>> Stashed changes
 
 #Initializing API address and API Key
 #Milplane Endpoint and Headers
@@ -20,14 +26,32 @@ def get_json_response(url, headers):
 #Iniitializing Fast App
 app = FastAPI()
 
+<<<<<<< Updated upstream
+=======
+# Obtain Refresh Times (In Seconds)
+@app.get("/times")
+async def refresh_times():
+    log_endpoint_access(logger, "times")
+    return {"ADS-B Calls": INTERVAL_SECONDS}
+    
+
+
+>>>>>>> Stashed changes
 #Homepage, this returns IEX HQ
 @app.get("/")
 async def index():
+<<<<<<< Updated upstream
     return get_json_response(IEX_ads, ads_headers)
+=======
+    log_endpoint_access(logger, "get_data")
+    return get_json_response(IEX_API_URL, ads_headers)
+>>>>>>> Stashed changes
+
 
 #MilPlane, This returns Milplane
 @app.get("/milplane")
 async def MilPlane():
+<<<<<<< Updated upstream
     return get_json_response(mil_ads, ads_headers)
 
 
@@ -35,3 +59,43 @@ async def MilPlane():
 
 #docker-compose build 
 #docker-compose up -d
+=======
+    log_endpoint_access(logger, "milplane")
+    return get_json_response(API_STARTER + "/mil/", ads_headers)
+
+# This returns aircraft around the selected number of 
+# nautical (nm) miles
+@app.get("/dist/{int_nm}")
+async def dist(int_nm: int):
+    log_endpoint_access(logger, "dist")
+    return get_json_response(API_STARTER + IEX_LAT_LON + f"/dist/{int_nm}", 
+                            ads_headers)                         
+
+# This returns aircraft based on the inputed icao
+@app.get("/icao/{icao}")
+async def icao(icao: str):
+    log_endpoint_access(logger, "icao")
+    return get_json_response(API_STARTER + f"/icao/{icao}", ads_headers)
+
+# This returns aircraft based on the inputed hex
+@app.get("/hex/{hex}")
+async def hex(hex: str):
+    log_endpoint_access(logger, "hex")
+    return get_json_response(API_STARTER + f"/hex/{hex}", ads_headers)
+
+# This returns aircraft based on the inputed squawk code
+@app.get("/squawk/{squawk}")
+async def squawk(squawk: str):
+    log_endpoint_access(logger, "squawk")
+    return get_json_response(API_STARTER + f"/sqk/{squawk}", 
+                                ads_headers)
+
+# This returns aircraft based on the inputed squawk code
+@app.get("/callsign/{callsign}")
+async def callsign(callsign: str):
+    log_endpoint_access(logger, "callsign")
+    return get_json_response(API_STARTER + f"/callsign/{callsign}", 
+                                ads_headers)
+
+
+>>>>>>> Stashed changes
